706546becb41d5b58799db1d0ca3c64e4cc9aaeb
clwu1994 <1445860318@qq.com>
2020-11-23 21:50:29 +0800

    feat: 添加组件属性

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+// 中划线转驼峰
+export function camelCase(str) {
+  return str.replace(/-[a-z]/g, str1 => str1.substr(-1).toUpperCase())
+}
+
+export function isNumberStr(str) {
+  return /^[+-]?(0|([1-9]\d*))(\.\d+)?$/g.test(str)
+}
+
+export function isArray(val) {
+  return Array.isArray(val)
+}
